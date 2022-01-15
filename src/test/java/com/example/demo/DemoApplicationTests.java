e96a2b30d2912b9224eb0ecf8fd9b69739d1bf5c
kboussaidi <b.khaled.mi@gmail.com>
2022-01-15 16:14:07 +0100

    setup java maven template project

---
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class DemoApplicationTests {
+
+	@Test
+	void works() {
+		Assertions.assertTrue(true);
+	}
+}
